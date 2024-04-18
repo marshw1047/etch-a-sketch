b8774e5532d4bcd0d4e4270ae4b0a2622c626ace
Marshall <42623637+marshalljwu@users.noreply.github.com>
2024-04-18 02:23:56 -0400

    Completed adding working changeable grid size and hover effect

--- javascript.js
+++ javascript.js
@@ -1,17 +1,36 @@
-function createBoard() {
-    let container = document.getElementById("container");
-    
+const container = document.getElementById("container"); 
 
-    for (let y = 0; y < 8; y++) {
+function createBoard() {
+    let size = prompt("Please enter a number 1-100 for grid size!");
+    while (isNaN(size) || size < 1 || size > 100) {
+        size = prompt("Invalid. Please enter a number 1-100 for grid size!");
+    }
+
+    newBoard();
+
+    for (let y = 0; y < size; y++) {
         var row = document.createElement("div");
         row.classList.add("row");
         
-        for (let x = 0; x < 8; x++) {
+        for (let x = 0; x < size; x++) {
             var squareRow = document.createElement("div");
             squareRow.classList.add("square");
             row.appendChild(squareRow);
         }
         container.appendChild(row);
     }
-        
+    
+    var squares = document.querySelectorAll(".square");
+    squares.forEach(function(square) {
+        square.addEventListener("mouseover", function(event) {
+            event.target.style.backgroundColor = "black";
+        })
+    })
+
+}
+
+function newBoard() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
